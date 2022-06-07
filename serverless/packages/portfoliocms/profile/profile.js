1f3fea77c8e8d65aaf2dd5dbae06561e25323cdc
esteban-gs <26440947+esteban-gs@users.noreply.github.com>
2022-06-07 00:20:57 -0700

    function implement new

--- serverless/packages/portfoliocms/profile/profile.js
+++ serverless/packages/portfoliocms/profile/profile.js
@@ -1,6 +1,15 @@
 require("dotenv").config({ path: "../../../" });
+const { Octokit } = require("@octokit/core");
 
-exports.main = (args) => {
+exports.main = async (args) => {
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+  const githubprofile = (await octokit.request(`GET /users/${process.env.GITHUB_USERNAME}`, {
+    username: 'USERNAME'
+  })).data;
+
+  console.log(githubprofile);
+
   let content = {
     profileName: process.env.PROFILE_NAME,
     linkedInUrl: process.env.LINKED_IN_URL,
@@ -8,6 +17,11 @@ exports.main = (args) => {
     githubUrl: process.env.GITHUB_URL,
     location: process.env.LOCATION,
     profileBackgroundImg: process.env.PROFILE_BACKGROUND_IMG,
+    githubProfile: {
+      public_repos: githubprofile.public_repos,
+      followers: githubprofile.followers,
+      following: githubprofile.following,
+    },
   };
   return { body: content };
 };
